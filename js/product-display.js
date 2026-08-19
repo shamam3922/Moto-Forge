eaa13b35dab5c45dab66c3b5f86335976aaf6b7f
shamam3922 <shamam3922@students.ecpi.edu>
2026-08-19 22:03:11 +0000

    Starting Code

---
+++ js/product-display.js
@@ -0,0 +1,5 @@
+const products = [
+    {name: "Bell MX-9 Adventure Helment", price: "199.99", category: "Helments"},
+    {name: "Alpinestars GP Plus R V2 Jacket", price: "349.99", category: "Jackets"},
+
+]
